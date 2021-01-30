52961033d13303b5ba20dfa237b0b0f5d0dc7f06
Zeliha Ural Merpez <zelhaaa@gmail.com>
2021-01-29 17:10:13 -0800

    Added all graphs together

--- app.R
+++ app.R
@@ -75,39 +75,42 @@ world_fig <- world_fig %>% layout(
     clickmode = 'event+select'
 )
 
-# Create connected graph function
-create_connected_graph <- function(df = merged_df, region = "Western Europe"){
-    region_df <- df %>%
-        filter(Regional_indicator == region)
-    
-    # Happiness rank plot for the selected region
-    happiness_rank <- ggplot(region_df) +
-        aes(x = Happiness_score,
-            y = reorder(Country, Happiness_score),
-            xmin = lowerwhisker,
-            xmax = upperwhisker) +
-        geom_point(color = "#0abab5", size = 3) +
-        geom_errorbarh(height = 0.2) +
-        labs(x = "Happiness Score", y = "")
-    happiness_rank <- happiness_rank +
-        theme_classic()
-    
-    # Population density chart for the selected region
-    density_chart <- ggplot(region_df) +
-        aes(x = Density, y = reorder(Country, Density)) +
-        geom_bar(stat = "identity", fill = "#0abab5") +
-        # scale_x_continuous(oob=scales::oob_keep) +
-        labs(x = "Density", y = "")
-    density_chart <- density_chart +
-        theme_classic()
-    
-    # Subplot of connected graph
-    subplot_conn <- subplot(ggplotly(happiness_rank) %>% layout(clickmode = 'event+select'),
-                            ggplotly(density_chart) %>% layout(clickmode = 'event+select'),
-                            margin = 0.08)
-    
-    return(subplot_conn)
+
+create_error_graph <- function(df = merged_df, region = "Western Europe"){
+  region_df <- df %>%
+      filter(Regional_indicator == region)
+  
+  # Happiness rank plot for the selected region
+  happiness_rank <- ggplot(region_df) +
+      aes(x = Happiness_score,
+          y = reorder(Country, Happiness_score),
+          xmin = lowerwhisker,
+          xmax = upperwhisker) +
+      geom_point(color = "#0abab5", size = 3) +
+      geom_errorbarh(height = 0.2) +
+      labs(x = "Happiness Score", y = "")
+  happiness_rank <- happiness_rank +
+      theme_classic() 
+  error <- ggplotly(happiness_rank) %>% layout(clickmode = 'event+select')
+  return(error)
 }
+
+create_bar_graph <- function(df = merged_df, region = "Western Europe"){
+  region_df <- df %>%
+    filter(Regional_indicator == region)
+  # Population density chart for the selected region
+  density_chart <- ggplot(region_df) +
+    aes(x = Density, y = reorder(Country, Density)) +
+    geom_bar(stat = "identity", fill = "#0abab5") +
+    # scale_x_continuous(oob=scales::oob_keep) +
+    labs(x = "Density", y = "")
+  density_chart <- density_chart +
+    theme_classic()
+ 
+  bar <- ggplotly(density_chart) %>% layout(clickmode = 'event+select')
+  return(bar)
+}
+
 
 app$layout(
     htmlDiv(list(
@@ -135,7 +138,7 @@ app$layout(
                 dbcLabel("Select Preferences"),
                 dccDropdown(
                     id = 'preference_name',
-                    value = 'Ladder score',
+                    value = 'Happiness_score',
                     options = preferences_indicator
                 ))
             ),
@@ -143,20 +146,44 @@ app$layout(
                 dbcLabel("Select Country"),
                 dccDropdown(
                     id = 'country_name',
-                    options = country_indicator
+                    options = country_indicator,
+                    multi = TRUE
                 ))
             )
         )),
         htmlBr(),
-        htmlDiv(list(
-            dccGraph(
-                id = 'world_map',
-                figure = world_fig)
+        htmlDiv(id ='data_table', list(
+            htmlDiv(list(
+                dccGraph(
+                    id = 'world_map',
+                    figure = world_fig)
+            )),
+            htmlDiv(id = 'col_2', list(
+                htmlDiv(list(
+                    dashDataTable(
+                        id = 'table'
+                )
+                )),
+                htmlDiv(list(
+                    dccGraph(
+                        id = 'preference_bar_plot',
+                    )
+                ))
+            ))
         )),
-        htmlDiv(list(
-            dccGraph(
-                id = 'connected_graph',
-                figure = create_connected_graph())
+        htmlDiv(id = 'row_4', list(
+            htmlDiv(list(
+                dccGraph(
+                    id = 'error_plot',
+                    figure = create_error_graph()
+                )
+            )),
+            htmlDiv(list(
+                dccGraph(
+                    id = 'bar_plot',
+                    figure = create_bar_graph()
+                )
+            ))
         ))
     ))
 )
@@ -171,11 +198,59 @@ app$callback(
         lapply(filtered_list, function(x) list(label = x, value = x))
 })
 
+
 app$callback(
-    output=list(id='connected_graph', property='figure'),
-    params=list(input(id='region', property='value')),
-    function(input_value) {
-        create_connected_graph(df = merged_df, region = input_value)
+    output=list(id='bar_plot', property='figure'),
+    params=list(input(id='region', property = 'value')),
+    function(input_value){
+        create_bar_graph(df = merged_df, region = input_value)
+})
+
+app$callback(
+    output=list(id='error_plot', property='figure'),
+    params=list(input(id='region', property = 'value')),
+    function(input_value){
+        create_error_graph(df = merged_df, region = input_value)
+})
+
+app$callback(
+    output=list(id='preference_bar_plot', property='figure'),
+    params=list(input(id='region', property='value'),
+                input(id='preference_name', property='value')),
+    function(region, column){
+      region_df <- df %>% filter( Region == region)
+      bar_plot <- ggplot(region_df) +
+        aes(x = !!sym(column),
+            fill = Country,
+            y = reorder(Country, !!sym(column))) +
+        geom_bar(stat = 'identity', fill="#0ABAB5") +
+                 scale_x_continuous(expand=expansion(mult=c(0,0.5))) +
+                   ylab("")
+                 print(region_df)
+    return(ggplotly(bar_plot))
+})
+
+###App callback for data
+app$callback(
+    output = list(id = 'table', property= 'data'),
+    params=list(input(id = 'country_name', property ='value')),
+    function(input_value = "Canada") {
+      country_df <- filter(df, Country %in% input_value) 
+      country_df 
+})
+
+###App callback for preferences
+app$callback(
+   output = list(id = 'table', property= 'columns'),
+   params=list(input(id = 'preference_name',property ='value')),
+   function(preference_value = 'Social support') {
+      col<-list( 
+        list(id = "Country", name = "Country"),
+        list(
+            id = preference_value,
+            name = str_replace_all(preference_value, "_"," ")
+        ))
+      col
 })
 
 app$run_server(debug = T)
